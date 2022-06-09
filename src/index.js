cd1d31e758d571d6d515795b7afebc155fec7553
Janosch <janosch@janosch.dev>
2022-06-09 23:42:30 +0200

    Some comments added, that you dont confuse while watching this really shit, old code

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const mysql = require('mysql');
 const schedule = require('node-schedule');
 
 var mysqlConnection = mysql.createConnection({
-    host: '192.168.178.130',
-    user: 'root',
-    password: 'raspberry',
-    database: 'mctracker'
+    host: '127.0.0.1',
+    user: 'USERNAME',
+    password: 'PASSWORD',
+    database: 'DATABASE'
 });
 
+// List with servers to ping, the code was private for a long time, so there are no big configuration options
 const servers = new Map([
     ['hypixel.net', 'casual'],
     ['gommehd.net', 'sweater'],
@@ -35,6 +36,7 @@ function requestServers() {
 
     servers.forEach(function(value, key, map) {
         RequestHandler.getData(key, function(data) {
+            // I know, you shouldn't do that
             var sql = "INSERT INTO serverstats (date, date_only, time_only, address, type, version, protocolversion, playercount, maxplayercount, favicon, ping) VALUES ('" + date_only + " " + time + "', '" + date_only + "', '" + time + "', '" + key + "', '" + value + "', '" + data.version.name + "', " + data.version.protocol + ", " + data.players.online + ", " + data.players.max + ", '" + "disabled" + "', " + data.ping + ")";
             console.log(date_only + ' | ' + time + ' | ' + 'Adding entry to db: ' + key + ' / ' + data.players.online + ' / ' + data.players.max + ' / ' + data.ping + 'ms');
             mysqlConnection.query(sql, function(err, result) {
